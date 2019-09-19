09448dc810e87dd8a850e4b66e607c68b3da8c69
zeh3 <zeh3@illinois.edu>
2019-09-18 22:03:20 -0500

    made it work (kinda) (with help)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        events: []
+      }
+    }  
+    componentDidMount() {
+      getEvents().then(json => {
+        console.log(json.events);
+        this.setState({events: json.events})
+      });
+      
+    }
+    render () {
+      
+      return (
+        <div>
+          <h1> Schedule </h1>
+          <p> welcome to hackillinois! </p>
 
-function App() {
+          <ul>{this.state.events.map(event => <li>{event.name}</li>)}</ul>
+        </div>
+      );
+    }
+  
+}
+function printMessage() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <p>this still doesn't connect to the api I'm very sorry</p>
+  )
+}
+//DO NOT CHANGE!!!!!!
+function getEvents() {
+  return fetch('https://api.hackillinois.org/event/', {
+    method: 'GET' ,
+    headers: {
+      'Content-Type' : 'applications/json'
+    }
+  }).then(res => {
+   return res.json();
+  });
 }
 
 export default App;
